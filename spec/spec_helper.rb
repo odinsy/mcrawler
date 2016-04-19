f27f2505e0bbe17475c57dc731654382115111b4
odianov <odianov@wks-14.co.spb.ru>
2016-04-19 12:46:00 +0300

    add gem 'factori_girl'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'metrics_crawler'
+require 'factory_girl'
+
+Dir["./spec/support/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.failure_color = :yellow
+end
