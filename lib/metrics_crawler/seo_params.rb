dfa8d560b6e622006f8171352f8d71ba407c4972
odianov <odianov@wks-14.co.spb.ru>
2016-04-21 14:46:25 +0300

    refactor

--- lib/metrics_crawler/seo_params.rb
+++ lib/metrics_crawler/seo_params.rb
@@ -7,14 +7,16 @@ module MetricsCrawler
   class SeoParams
     include ConnectionChecker
 
-    YACA_LINK           = 'https://yandex.ru/yaca/?text='.freeze
-    PRCY_LINK           = 'http://pr-cy.ru/a'.freeze
-    LINKPAD_LINK        = 'https://www.linkpad.ru/?search='.freeze
-    DMOZ_LINK           = 'https://www.dmoz.org/search?q='.freeze
-    ALEXA_LINK          = 'http://www.alexa.com/siteinfo'.freeze
-    PRCY_YAINDEX_XPATH  = './/div[@id="box-basik"][1]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"][1]//div[@class="pull-right"][3]//a'.freeze
-    PRCY_GOINDEX_XPATH  = './/div[@id="box-basik"][2]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"][1]//div[@class="pull-right"][3]//a'.freeze
-    PRCY_HOSTINFO_XPATH = './/div[@id="box-basik"][4]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"]//div[@class="pull-right"]'.freeze
+    YACA_LINK               = 'https://yandex.ru/yaca/?text='.freeze
+    PRCY_LINK               = 'http://pr-cy.ru/a'.freeze
+    LINKPAD_LINK            = 'https://www.linkpad.ru/?search='.freeze
+    DMOZ_LINK               = 'https://www.dmoz.org/search?q='.freeze
+    ALEXA_LINK              = 'http://www.alexa.com/siteinfo'.freeze
+    PRCY_YANDEX_INDEX_XPATH = './/div[@id="box-basik"][1]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"][1]//div[@class="pull-right"][3]//a'.freeze
+    PRCY_YANDEX_CAT_XPATH   = './/div[@id="box-basik"][1]/div[2]/div/div[1]/div[2]/a'.freeze
+    PRCY_YANDEX_TIC_XPATH   = './/div[@id="box-basik"][1]/div[2]/div/div[1]/div[1]/a'.freeze
+    PRCY_GOOGLE_INDEX_XPATH = './/div[@id="box-basik"][2]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"][1]//div[@class="pull-right"][3]//a'.freeze
+    PRCY_HOSTINFO_XPATH     = './/div[@id="box-basik"][4]//div[@class="box-content"]//div[@class="row"]//div[@class="col-sm-4"]//div[@class="pull-right"]'.freeze
 
     def initialize(url, proxy = nil)
       @proxy  = URI.parse(URI.encode(proxy)) unless proxy.nil?
@@ -23,30 +25,31 @@ module MetricsCrawler
 
     def all
       begin
-        yaca      = Nokogiri::HTML(open("#{YACA_LINK}#{@url}", proxy: @proxy, read_timeout: 15))
-        doc_prcy  = Nokogiri::HTML(open("#{PRCY_LINK}/#{@url}", proxy: @proxy, read_timeout: 15))
-        host_info = get_host_info(doc_prcy)
+        yaca      = Nokogiri::HTML(open("#{YACA_LINK}#{@url}", proxy: @proxy, read_timeout: 20))
+        PageRankr.proxy_service = PageRankr::ProxyServices::Random.new(@proxy.to_s) unless @proxy.nil?
+        doc_prcy  = Nokogiri::HTML(open("#{PRCY_LINK}/#{@url}", proxy: @proxy, read_timeout: 20))
+        host_info = host_info(doc_prcy)
         result    = {
           proxy:            @proxy.to_s,
           url:              @url,
-          yandex_catalog:   get_yandex_catalog(yaca),
-          tic:              get_yandex_tic(yaca),
-          yandex_index:     get_yandex_index(doc_prcy),
-          backlinks:        get_backlinks(@url),
-          goggle_pagerank:  get_google_pagerank(@url),
-          google_index:     get_google_index(doc_prcy),
-          dmoz_catalog:     get_dmoz_catalog(@url),
-          alexa_rank:       get_alexa_rank(@url),
+          yandex_catalog:   yandex_catalog(doc_prcy),
+          tic:              yandex_tic(doc_prcy),
+          yandex_index:     yandex_index(doc_prcy),
+          google_index:     google_index(doc_prcy),
+          google_pagerank:  google_pagerank(@url),
+          backlinks:        backlinks(@url),
+          dmoz_catalog:     dmoz_catalog(@url),
+          alexa_rank:       alexa_rank(@url),
           host_age:         host_info[0],
           host_ip:          host_info[1],
           host_country:     host_info[3],
           host_from:        host_info[4],
           host_to:          host_info[5],
-          download_speed:   get_benchmarking(@url),
-          external_links:   get_external_links(@url)
+          download_speed:   benchmarking(@url),
+          external_links:   external_links(@url)
         }
       rescue => ex
-        error_handler("#{ex.class} #{ex.message} #{@url} #{@proxy}")
+        error_handler("#{ex.class} #{ex.message} #{@url} #{@proxy} #{result}")
         # exit
       end
       result
@@ -54,56 +57,57 @@ module MetricsCrawler
 
     private
 
-    def get_yandex_catalog(doc)
-      response_sites = doc.css('.yaca-snippet__cy')
-      response_sites.count > 0 ? true : false
+    def yandex_catalog(doc_prcy)
+      doc_prcy.xpath(PRCY_YANDEX_CAT_XPATH).text.strip == 'Да' ? true : false
     end
 
-    def get_yandex_tic(doc)
-      response_sites = doc.css('.yaca-snippet__cy')
-      response_sites.count > 0 ? response_sites.first.text.gsub(/ТИЦ: /, '') : 'Null'
+    def yandex_tic(doc_prcy)
+      doc_prcy.xpath(PRCY_YANDEX_TIC_XPATH).text.strip
     end
 
-    def get_yandex_index(doc_prcy)
-      yandex_index = doc_prcy.xpath(PRCY_YAINDEX_XPATH).text.strip
+    def yandex_index(doc_prcy)
+      yandex_index = doc_prcy.xpath(PRCY_YANDEX_INDEX_XPATH).text.strip
       yandex_index.gsub(/n\/a/, 'Null')
     end
 
-    def get_google_index(doc_prcy)
-      google_index = doc_prcy.xpath(PRCY_GOINDEX_XPATH).text.strip
+    def google_index(doc_prcy)
+      google_index = doc_prcy.xpath(PRCY_GOOGLE_INDEX_XPATH).text.strip
       google_index.gsub(/n\/a/, 'Null')
     end
 
-    def get_google_pagerank(url)
-      PageRankr.proxy_service = PageRankr::ProxyServices::Random.new(@proxy.to_s) unless @proxy.nil?
-      pagerank = PageRankr.ranks(url, :google)[:google]
-      pagerank.nil? ? 'Null' : pagerank
+    def google_pagerank(url)
+      tracker = PageRankr::Ranks::Google.new(url)
+      tracker.run
     end
 
-    def get_backlinks(url)
-      PageRankr.proxy_service = PageRankr::ProxyServices::Random.new(@proxy.to_s) unless @proxy.nil?
+    # def get_google_pagerank(url)
+    #   PageRankr.proxy_service = PageRankr::ProxyServices::Random.new(@proxy.to_s) unless @proxy.nil?
+    #   pagerank = PageRankr.ranks(url, :google)[:google]
+    #   pagerank.nil? ? 'Null' : pagerank
+    # end
+
+    def backlinks(url)
       backlinks = PageRankr.backlinks(url, :google)[:google]
     rescue SocketError
       error_handler "SocketError: не получилось узнать количество backlinks для #{url}"
       'Null'
     else
-      backlinks = 'Null' if backlinks.nil?
-      backlinks
+      backlinks.nil? ? 'Null' : backlinks
     end
 
-    def get_dmoz_catalog(url)
-      doc = Nokogiri::HTML(open("#{DMOZ_LINK}#{url}", proxy: @proxy, read_timeout: 15))
+    def dmoz_catalog(url)
+      doc = Nokogiri::HTML(open("#{DMOZ_LINK}#{url}", proxy: @proxy, read_timeout: 20))
       response_dmoz = doc.css('.open-dir-sites')
       response_dmoz.empty? ? false : true
     end
 
-    def get_alexa_rank(url)
-      doc = Nokogiri::HTML(open("#{ALEXA_LINK}/#{url}", proxy: @proxy, read_timeout: 15))
+    def alexa_rank(url)
+      doc = Nokogiri::HTML(open("#{ALEXA_LINK}/#{url}", proxy: @proxy, read_timeout: 20))
       alexa_rank = doc.css('.metrics-data.align-vmiddle').first.text.strip.delete(',').to_i
       alexa_rank.zero? ? 'Null' : alexa_rank
     end
 
-    def get_host_info(doc_prcy)
+    def host_info(doc_prcy)
       result = []
       begin
         registration_info = doc_prcy
@@ -118,7 +122,7 @@ module MetricsCrawler
       result
     end
 
-    def get_benchmarking(url)
+    def benchmarking(url)
       dt = `curl -o /dev/null -s -w %{time_total} 'http://#{url}'`
     rescue Net::ReadTimeout => ex
       error_handler("#{ex.class} #{ex.message}")
@@ -127,8 +131,8 @@ module MetricsCrawler
       "#{(dt.tr(',', '.').to_f * 100).ceil}ms"
     end
 
-    def get_external_links(url)
-      external_links = Nokogiri::HTML(open("#{LINKPAD_LINK}#{url}", proxy: @proxy, read_timeout: 15))
+    def external_links(url)
+      external_links = Nokogiri::HTML(open("#{LINKPAD_LINK}#{url}", proxy: @proxy, read_timeout: 20))
     rescue SocketError, Errno::ETIMEDOUT => ex
       error_handler("#{ex.class} #{ex.message}")
       'Null'
@@ -145,8 +149,8 @@ module MetricsCrawler
     private
 
     def prepare_url(url)
-      url.gsub(/(http:\/\/|https:\/\/)/, '')
-      url.chomp!('/') if @url[-1] == '/'
+      url.gsub!(/(http:\/\/|https:\/\/)/, '')
+      url.chomp!('/') if url[-1] == '/'
       URI.parse(URI.encode(url)).to_s
     end
   end
