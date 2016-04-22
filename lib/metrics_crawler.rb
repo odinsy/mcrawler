2e1e27486d2eb1e0b0af90e33febbc5ca6ddbea1
odianov <odianov@wks-14.co.spb.ru>
2016-04-22 11:58:51 +0300

    fix list of libraries

--- lib/metrics_crawler.rb
+++ lib/metrics_crawler.rb
@@ -1,8 +1,9 @@
-require_relative "metrics_crawler/version.rb"
-require_relative "metrics_crawler/crawler.rb"
-require_relative "metrics_crawler/seo_params.rb"
-require_relative "metrics_crawler/proxy_fetcher.rb"
-require_relative 'metrics_crawler/connection_checker.rb'
+require_relative "metrics_crawler/version"
+require_relative "metrics_crawler/crawler"
+require_relative "metrics_crawler/seo_params"
+require_relative "metrics_crawler/splitter"
+require_relative "metrics_crawler/export"
+require_relative 'metrics_crawler/connection_checker'
 
 module MetricsCrawler
 end
