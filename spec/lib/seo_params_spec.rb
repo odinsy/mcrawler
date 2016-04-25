6505218b6c86f0cb99ef8f01680fc72bdc4ba95c
odianov <odianov@wks-14.co.spb.ru>
2016-04-25 11:34:27 +0300

    start code coverage via rspec

--- spec/lib/seo_params_spec.rb
+++ spec/lib/seo_params_spec.rb
@@ -1,29 +1,46 @@
 require 'metrics_crawler/seo_params'
 
-describe "SeoParams" do
+describe 'SeoParams' do
   let(:seo_params) { build(:seo_params) }
+  let(:seo_params_with_proxy) { build(:seo_params_with_proxy) }
   let(:url) { 'https://www.facebook.com/' }
   let(:proxy) { 'http://wls-01.co.spb.ru:3128' }
 
   context 'without proxy' do
-    describe ".new" do
+    describe '.new' do
       it 'creates new instance' do
         expect(MetricsCrawler::SeoParams.new(url)).to be_a(MetricsCrawler::SeoParams)
       end
-    end
-    describe 'all' do
-      it 'returns a Hash' do
-        expect(seo_params.all).to be_a(Hash)
+      it 'makes an URL shorter' do
+        expect(seo_params.url).to eq('www.facebook.com')
       end
-      it 'returns information about domain' do
+    end
 
+    describe 'all', vcr: true do
+      attributes = %w(url yandex_catalog yandex_tic yandex_index google_index google_pagerank backlinks dmoz_catalog alexa_rank host_age host_ip host_country host_from host_to download_speed external_links)
+      subject(:result) { seo_params.all }
+
+      it 'responds to method "all"' do
+        expect(seo_params).to respond_to(:all)
+      end
+      it 'returns a Hash' do
+        expect(result).to be_a(Hash)
+      end
+      attributes.each do |key|
+        it "returns result with a key #{key}" do
+          expect(result).to have_key(key.to_sym)
+        end
       end
     end
   end
+
   context 'with proxy' do
-    describe ".new" do
+    describe '.new' do
       it 'creates new instance' do
         expect(MetricsCrawler::SeoParams.new(url, proxy)).to be_a(MetricsCrawler::SeoParams)
+      end
+      it 'have a class URI::HTTP for the attribute PROXY' do
+        expect(seo_params_with_proxy.proxy).to be_a(URI::HTTP)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'metrics_crawler'
 require 'factory_girl'
+require 'webmock/rspec'
+require 'vcr'
 
 Dir["./spec/support/*.rb"].each {|f| require f}
+
+VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = true
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.failure_color = :yellow
