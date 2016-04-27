6f1bbc80ef5416a541805eadcb17b57a7df88efb
odianov <odianov@wks-14.co.spb.ru>
2016-04-27 18:56:56 +0300

    fix method

--- lib/metrics_crawler/export.rb
+++ lib/metrics_crawler/export.rb
@@ -6,7 +6,7 @@ module MetricsCrawler
     # Exports array with movie hashes to JSON
     # @return [String]  the filename
     def save_to_json(hash, filename = './data/results/domains.json')
-      make_results_dir
+      make_dir('./data/results')
       File.open(filename, 'a+') { |f| f.puts hash.to_json }
       filename
     end
@@ -14,7 +14,7 @@ module MetricsCrawler
     # Exports array with movie hashes to CSV
     # @return [String]  the filename
     def save_to_csv(hash, filename = './data/results/domains.csv')
-      make_results_dir
+      make_dir('./data/results')
       CSV.open(filename, 'a+', col_sep: '|') do |file|
         file << hash.values.map { |v| v.is_a?(Array) ? v.join(',') : v }
       end
@@ -22,8 +22,8 @@ module MetricsCrawler
     end
 
     # Creates directory tree unless not exists
-    def make_results_dir
-      FileUtils::mkdir_p 'data/results' unless File.exists?('data/results')
+    def make_dir(dir)
+      FileUtils::mkdir_p(dir) unless File.exists?(dir)
     end
   end
 end
