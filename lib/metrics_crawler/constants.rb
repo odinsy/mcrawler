558945789ed0e4e737a4a163b0b7d45ec76819ff
odianov <odianov@wks-14.co.spb.ru>
2016-05-04 18:57:13 +0300

    constants moved to separate file

---
+++ lib/metrics_crawler/constants.rb
@@ -0,0 +1,10 @@
+require_relative 'application'
+
+module MetricsCrawler
+  APPLICATION   = "metrics_crawler"
+  TMP_PATH      = "/tmp/#{APPLICATION}".freeze
+  DEFAULT_CONF  = "#{root}/config/config.yml".freeze
+  CONFIG_PATH   = ENV['HOME'] + "/.config/#{APPLICATION}/config.yml".freeze
+  TIMESTAMP     = `date +%Y%m%d%H%M`.to_s.strip.freeze
+  RESULT_FILE   = "#{TIMESTAMP}-res.csv".freeze
+end
