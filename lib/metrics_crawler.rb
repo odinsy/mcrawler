dc272e574887de33f3b9510bb652cd84a5886e28
odianov <odianov@wks-14.co.spb.ru>
2016-05-04 18:58:58 +0300

    deleted extra

--- lib/metrics_crawler.rb
+++ lib/metrics_crawler.rb
@@ -1,9 +1,14 @@
-require_relative "metrics_crawler/version"
-require_relative "metrics_crawler/crawler"
-require_relative "metrics_crawler/seo_params"
-require_relative "metrics_crawler/splitter"
-require_relative "metrics_crawler/export"
+require_relative 'metrics_crawler/application'
+require_relative 'metrics_crawler/version'
+require_relative 'metrics_crawler/constants'
+require_relative 'metrics_crawler/seo_params'
+require_relative 'metrics_crawler/crawler'
+require_relative 'metrics_crawler/splitter'
+require_relative 'metrics_crawler/export'
 require_relative 'metrics_crawler/connection_checker'
+require_relative 'metrics_crawler/config'
+require_relative 'metrics_crawler/cli'
+require_relative 'metrics_crawler/cli/start'
 
 module MetricsCrawler
 end
