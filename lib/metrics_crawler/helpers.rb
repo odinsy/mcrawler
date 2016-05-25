dd0be628da39c6bf35f6dd2ec3ffd6eeeb3a8f2a
odinsy <odidoit@gmail.com>
2016-05-25 23:10:24 +0300

    move more methods to the module Helpers

--- lib/metrics_crawler/helpers.rb
+++ lib/metrics_crawler/helpers.rb
@@ -4,10 +4,22 @@ module MetricsCrawler
     def make_dir(dir)
       FileUtils.mkdir_p(dir) unless File.exist?(dir)
     end
-    
+    # Загружает домены из файла
     def load_domains(path)
       raise ArgumentError, "File #{path} not found." unless File.exist?(path)
       File.readlines(path).map(&:strip)
+    end
+    # Подготовка имени ноды - берется только hostname.
+    def prepare_nodes(nodes)
+      nodes.map { |node| check_uri(node).host }
+    end
+    # Проверяет URI на корректность.
+    # Если URI не содержит hostname или port, выдаст исключение.
+    # В противном случае возвращает URI
+    def check_uri(uri)
+      uri = Addressable::URI.parse(uri)
+      raise ArgumentError, "Node #{uri} has a bad URI." if uri.host.nil? || uri.port.nil?
+      uri
     end
   end
 end
