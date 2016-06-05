dd6f42246204dd3ae494bc5152b9228eb64220e5
odinsy <odidoit@gmail.com>
2016-06-05 14:55:23 +0300

    delete extra method

--- lib/metrics_crawler/nodes.rb
+++ lib/metrics_crawler/nodes.rb
@@ -1,18 +1,8 @@
-require "addressable/uri"
-
 module MetricsCrawler
   module Nodes
     # Подготовка нод
     def prepare_nodes(nodes)
       nodes.map { |node| URI.parse(URI.encode(node)).to_s }
-    end
-    # Проверяет URI на корректность.
-    # Если URI не содержит hostname или port, выдаст исключение.
-    # В противном случае возвращает URI
-    def make_uri(uri)
-      uri = Addressable::URI.parse(uri)
-      raise ArgumentError, "Node #{uri} has a bad URI." if uri.host.nil? || uri.port.nil?
-      uri
     end
   end
 end
