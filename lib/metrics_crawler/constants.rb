6f8a30fdb522725dc528b88e3bafbd0be98dd37a
Oleg Dianov <odidoit@gmail.com>
2017-06-02 17:32:43 +0300

    delete extra constants

--- lib/metrics_crawler/constants.rb
+++ lib/metrics_crawler/constants.rb
@@ -5,6 +5,4 @@ module MetricsCrawler
   TMP_PATH      = "/tmp/#{APPLICATION}".freeze
   DEFAULT_CONF  = "#{root}/config/config.yml".freeze
   CONFIG_PATH   = ENV['HOME'] + "/.config/#{APPLICATION}/config.yml".freeze
-  TIMESTAMP     = `date +%Y%m%d%H%M`.to_s.strip.freeze
-  RESULT_FILE   = "#{TIMESTAMP}-res.csv".freeze
 end
